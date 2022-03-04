ede951d0ff0eb673d2f927dd7791ab37e39a858a
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2022-03-05 00:25:33 +0700

    Create flashcards fixtures

--- src/DataFixtures/FlashcardFixtures.php
+++ src/DataFixtures/FlashcardFixtures.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Flashcard;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class FlashcardFixtures extends Fixture
 {
+    private int $batchSize = 34;
+
     public function load(ObjectManager $manager): void
     {
-        
+        $faker = Factory::create();
 
-         $manager->persist($product);
+        for ($i = 1; $i <= 1024; $i++) {
+            $manager->persist(new Flashcard(
+                $faker->word,
+                $faker->word,
+            ));
+
+            if (($this->batchSize % $i) === 0) {
+                $manager->flush();
+                $manager->clear();
+            }
+        }
 
         $manager->flush();
     }
